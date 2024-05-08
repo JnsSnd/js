4c413cb3267c65eb8d3f91765d8f9b394a9604bf
JnsSnd <sandejonas.bsit@gmail.com>
2024-05-08 21:49:35 +0800

    deploy

---
+++ index.php
@@ -0,0 +1,504 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>JONAS SANDE</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Animation -->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Valera
+  * Template URL: https://bootstrapmade.com/valera-free-bootstrap-theme/
+  * Updated: Mar 17 2024 with Bootstrap v5.3.3
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+  <style>
+    @keyframes typewriter {
+      0% {
+        width: 0;
+      }
+
+      25% {
+        width: 100%;
+      }
+
+      75% {
+        width: 100%;
+      }
+
+      100% {
+        width: 0;
+      }
+    }
+
+    h2.typewriter {
+      overflow: hidden;
+      /* border-right: 2px solid #f0f0f0; */
+      white-space: nowrap;
+      animation: typewriter 6s steps(40, end) infinite;
+    }
+
+
+    @keyframes switchText {
+
+      0%,
+      50% {
+        content: "BACK END DEVELOPER";
+      }
+
+      50.01%,
+      100% {
+        content: "COMPUTER & LAPTOP TECHNICIAN";
+      }
+    }
+
+    h2.typewriter::before {
+      content: "BACK END DEVELOPER";
+      display: inline-block;
+      animation: switchText 12s infinite;
+    }
+
+    @keyframes fadeIn {
+      0% {
+        opacity: 0;
+      }
+
+      100% {
+        opacity: 1;
+      }
+    }
+
+    .fade-in {
+      animation: fadeIn 2s ease-in-out;
+    }
+  </style>
+</head>
+
+<body>
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero">
+    <div class="hero-container">
+      <h4>HELLO!</h4>
+      <h1 class="fade-in" id="myName">I'M JONAS SANDE</h1>
+      <h2 class="typewriter"></h2>
+      <a href="#about" class="btn-get-started scrollto">Get Started</a>
+    </div>
+  </section>
+  <!-- End Hero -->
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="d-flex align-items-center ">
+    <div class="container-fluid d-flex align-items-center justify-content-lg-between">
+
+      <h1 class="logo me-auto me-lg-0"><a href="index.php">JONAS</a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo me-auto me-lg-0"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      <nav id="navbar" class="navbar order-last order-lg-0">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+          <li><a class="nav-link scrollto" href="#about">About</a></li>
+          <li><a class="nav-link scrollto" href="#services">Services</a></li>
+          <li><a class="nav-link scrollto" href="#skills">Skills</a></li>
+          <li><a class="nav-link scrollto " href="#portfolio">Projects</a></li>
+          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+      <div class="header-social-links d-flex align-items-center">
+        <!-- <a href="#" class="twitter"><i class="bi bi-twitter"></i></a> -->
+        <a href="https://www.facebook.com/jonas.sande.5" target="_blank" class="facebook"><i class="bi bi-facebook"></i></a>
+        <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+        <a href="https://www.linkedin.com/in/jonas-sande-8b9461303/" target="_blank" class="linkedin"><i class="bi bi-linkedin"></i></i></a>
+      </div>
+
+    </div>
+  </header><!-- End Header -->
+
+  <main id="main">
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+      <div class="container">
+
+        <div class="section-title" data-aos="fade-up">
+          <h2>About Me</h2>
+        </div>
+
+        <div class="row content">
+          <p style="text-align: center; font-size: 16px" data-aos="fade-up" data-aos-delay="100">
+            My name is Jonas Sande. I am 20 years old. I am a third-year student residing in Caloocan City, pursuing a Bachelor of Science in Information Technology (BSIT). At the University of Caloocan City (South Campus). I'm a kind, friendly, and amiable person who works best both in teams and alone since I like to establish and achieve goals for myself. I also have a creative mind and am open to taking on new challenges. I consistently plan ahead and maintain good organization to ensure that I make the best use of my time.
+          </p>
+        </div>
+
+        <br><br><br>
+        <div class="section-title" data-aos="fade-up" data-aos-delay="200">
+          <h2>Education</h2>
+        </div>
+        <div class="row content">
+          <!-- ERES -->
+          <div class="schoolCard" data-aos="fade-up" data-aos-delay="250">
+            <div class="img-wrapper">
+              <a href="https://example.com">
+                <img src="assets/img/schoolLogo/eres.png" class="img" alt="Your Image" />
+              </a>
+            </div>
+            <div class="textBox">
+              <br>
+              <p>E R E S<br> 2009 - 2015</p>
+            </div>
+          </div>
+
+          <!-- MCHS -->
+          <div class="schoolCard" data-aos="fade-up" data-aos-delay="300">
+            <div class="img-wrapper">
+              <a href="https://example.com">
+                <img src="assets/img/schoolLogo/mchs.png" class="img" alt="Your Image" />
+              </a>
+            </div>
+            <div class="textBox">
+              <br>
+              <p>M C H S<br> 2015 - 2019</p>
+            </div>
+          </div>
+
+          <!-- SPCC -->
+          <div class="schoolCard" data-aos="fade-up" data-aos-delay="400">
+            <div class="img-wrapper">
+              <a href="https://example.com">
+                <img src="assets/img/schoolLogo/spcc.png" class="img" alt="Your Image" />
+              </a>
+            </div>
+            <div class="textBox">
+              <br>
+              <p>S P C C<br> 2019 - 2021</p>
+            </div>
+          </div>
+
+          <!-- UCC -->
+          <div class="schoolCard" data-aos="fade-up" data-aos-delay="450">
+            <div class="img-wrapper">
+              <a href="https://example.com">
+                <img src="assets/img/schoolLogo/ucc.png" class="img" alt="Your Image" />
+              </a>
+            </div>
+            <div class="textBox">
+              <br>
+              <p>U C C<br> 2021 - PRESENT</p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+      </div>
+    </section><!-- End About Section -->
+
+    <!-- ======= Services Section ======= -->
+    <section id="services" class="services">
+      <div class="container">
+
+        <div class="section-title" data-aos="fade-up">
+          <h2 class="animate-title">Services</h2>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-4 col-md-6">
+            <div class="icon-box" data-aos="fade-up" data-aos-delay="100">
+              <div class="icon"><i class="bi bi-code-slash" style="color: #f0f0f0;"></i></div>
+              <h4 class="title"><a href="">Web Development</a></h4>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6 mt-4 mt-md-0">
+            <div class="icon-box" data-aos="fade-up" data-aos-delay="150">
+              <div class="icon"><i class="bi bi-microsoft" style="color: #f0f0f0;"></i></div>
+              <h4 class="title"><a href="">Windows Application Development</a></h4>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 mt-4 mt-lg-0">
+            <div class="icon-box" data-aos="fade-up" data-aos-delay="200">
+              <div class="icon"><i class="bi bi-tools" style="color: #f0f0f0;"></i></div>
+              <h4 class="title"><a href="">Computer & Laptop Technician</a></h4>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End Services Section -->
+
+
+    <!-- ======= Skills Section ======= -->
+    <section id="skills" class="clients section-bg" data-aos="fade-up">
+      <div class="container">
+        <div class="section-title">
+          <br>
+          <br>
+          <h2>Skills</h2>
+        </div>
+        <div class="row">
+
+          <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center" data-aos="fade-up" data-aos-delay="100">
+            <img src="assets/img/langLogo/c++.png" class="img-fluid" alt="">
+          </div>
+
+          <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center" data-aos="fade-up" data-aos-delay="150">
+            <img src="assets/img/langLogo/Csharp_Logo.png" class="img-fluid" alt="">
+          </div>
+
+          <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center" data-aos="fade-up" data-aos-delay="200">
+            <img src="assets/img/langLogo/vb.net.png" class="img-fluid" alt="">
+          </div>
+
+          <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center" data-aos="fade-up" data-aos-delay="250">
+            <img src="assets/img/langLogo/php.png" class="img-fluid" alt="">
+          </div>
+
+          <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center" data-aos="fade-up" data-aos-delay="300">
+            <img src="assets/img/langLogo/mytsql.png" class="img-fluid" alt="">
+          </div>
+
+          <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center" data-aos="fade-up" data-aos-delay="350">
+            <img src="assets/img/langLogo/bootstrap.png" class="img-fluid" alt="">
+          </div>
+
+          <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center" data-aos="fade-up" data-aos-delay="400">
+            <img src="assets/img/langLogo/html.png" class="img-fluid" alt="">
+          </div>
+
+          <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center" data-aos="fade-up" data-aos-delay="450">
+            <img src="assets/img/langLogo/css.png" class="img-fluid" alt="">
+          </div>
+
+          <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center" data-aos="fade-up" data-aos-delay="500">
+            <img src="assets/img/langLogo/js.png" class="img-fluid" alt="">
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Clients Section -->
+
+    <!-- ======= Projects Section ======= -->
+    <section id="portfolio" class="portfolio">
+      <div class="container">
+
+        <div class="section-title">
+          <h2 data-aos="fade-up">Projects</h2>
+          <p data-aos="fade-up" data-aos-delay="200" style="font-size: 16px;">These are my personal and academic projects.</p>
+        </div>
+
+        <!-- <div class="row portfolio-container">
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <div class="portfolio-img"><img src="assets/img/portfolio/portfolio-3.jpg" class="img-fluid" alt=""></div>
+            <div class="portfolio-info">
+              <h4>App 2</h4>
+              <p>App</p>
+              <a href="assets/img/portfolio/portfolio-3.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="App 2"><i class="bx bx-plus"></i></a>
+              <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
+            </div>
+          </div>
+        </div> -->
+
+        <!-- CARD -->
+        <div class="cardContainer">
+          <!-- PROJECT 1 -->
+          <div class="card" data-aos="fade-up" data-aos-delay="100">
+            <img src="assets/img/projLogo/jc.png" alt="Project Image" class="card__image">
+            <div class="card__content">
+              <!-- <p class="card__title">JC Thai Inventory Management System</p> -->
+              <a href="#" class="card__title">JC Thai Inventory Management System</a>
+            </div>
+          </div>
+
+
+
+          <!-- PROJECT 2 -->
+          <div class="card" data-aos="fade-up" data-aos-delay="150">
+            <img src="assets/img/projLogo/coftea.png" alt="Project Image" class="card__image">
+            <div class="card__content">
+              <!-- <p class="card__title">Coftea Computerized Point Of Sale System At A. Mabini Branch</p> -->
+              <a href="#" class="card__title">Coftea Computerized Point Of Sale System At A. Mabini Branch</a>
+            </div>
+          </div>
+
+          <!-- PROJECT 3 -->
+          <div class="card" data-aos="fade-up" data-aos-delay="200">
+            <img src="assets/img/projLogo/blk.png" alt="Project Image" class="card__image">
+            <div class="card__content">
+              <!-- <p class="card__title">Order And Stock Management System For Black Apparel</p> -->
+              <a href="#" class="card__title">Order And Stock Management System For Black Apparel</a>
+            </div>
+          </div>
+
+          <!-- PROJECT 4 -->
+          <div class="card" data-aos="fade-up" data-aos-delay="250">
+            <img src="assets/img/projLogo/chc.png" alt="Project Image" class="card__image">
+            <div class="card__content">
+              <!-- <p class="card__title">CHC: Cristy Homecare Corp</p> -->
+              <a href="#" class="card__title">CHC: Cristy Homecare Corp</a>
+            </div>
+          </div>
+
+          <!-- PROJECT 5 -->
+          <div class="card" data-aos="fade-up" data-aos-delay="300">
+            <img src="assets/img/projLogo/cornerWhoops.png" alt="Project Image" class="card__image">
+            <div class="card__content">
+              <!-- <p class="card__title">Corner Whoops Arduino Powered Pay Parking Management System</p> -->
+              <a href="#" class="card__title">Corner Whoops Arduino Powered Pay Parking Management System</a>
+            </div>
+          </div>
+        </div>
+
+
+      </div>
+    </section><!-- End Portfolio Section -->
+
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact">
+      <div class="container">
+
+        <div class="section-title">
+          <h2 data-aos="fade-up">Contact</h2>
+          <p data-aos="fade-up" data-aos-delay="100" style="font-size: 16px;">Send me an email if you would want to work on a case study, system, or capstone. This is my contact or complete the text field below.</p>
+        </div>
+
+        <!-- <div>
+          <iframe style="border:0; width: 100%; height: 270px;"
+            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621"
+            frameborder="0" allowfullscreen></iframe>
+        </div> -->
+
+        <div class="row mt-5">
+
+          <div class="col-lg-4" data-aos="fade-up" data-aos-delay="200">
+            <div class="info">
+
+              <div class="email">
+                <i class="bi bi-envelope"></i>
+                <h4>Email:</h4>
+                <p>sandejonas.bsit@gmail.com</p>
+              </div>
+
+              <div class="phone">
+                <i class="bi bi-phone"></i>
+                <h4>Call:</h4>
+                <p>09336609682</p>
+                <p>09942339256</p>
+              </div>
+
+            </div>
+
+          </div>
+
+          <div class="col-lg-8 mt-5 mt-lg-0" data-aos="fade-up" data-aos-delay="100">
+
+            <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+              <div class="row">
+                <div class="col-md-6 form-group">
+                  <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
+                </div>
+                <div class="col-md-6 form-group mt-3 mt-md-0">
+                  <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
+                </div>
+              </div>
+              <div class="form-group mt-3">
+                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
+              </div>
+              <div class="form-group mt-3">
+                <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
+              </div>
+              <div class="my-3">
+                <div class="loading">Loading</div>
+                <div class="error-message"></div>
+                <div class="sent-message">Your message has been sent. Thank you!</div>
+              </div>
+              <div class="text-center"><button type="submit">Send Message</button></div>
+            </form>
+
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Contact Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="container footer-bottom clearfix">
+      <div class="copyright">
+        &copy; Copyright <strong><span>JONAS</span></strong>. All Rights Reserved
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- COVER ANIMATION -->
+  <script>
+    // Wait for the DOM to fully load
+    document.addEventListener("DOMContentLoaded", function() {
+      // Get the <h2> element with the typewriter class
+      const typewriterElement = document.querySelector("h2.typewriter");
+
+      // Trigger the typewriter animation after a delay
+      setTimeout(function() {
+        typewriterElement.classList.add("start-typewriter");
+      }, 2000); // Adjust the delay (in milliseconds) as needed
+    });
+  </script>
+
+
+
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+  <!-- Animation Link -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>
+  <script>
+    AOS.init();
+  </script>
+
+</body>
+
+</html>
